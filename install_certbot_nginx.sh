027cfc3d574fe1bcd8cd3c8c1500546e9423f425
lpisces <iamalazyrat@gmail.com>
2018-12-07 12:51:56 +0800

    Update install_certbot_nginx.sh

--- install_certbot_nginx.sh
+++ install_certbot_nginx.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 docker pull certbot/certbot
-docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" certbot/certbot certbot-auto certonly --manual
+docker run -it --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" certbot/certbot certonly --manual
